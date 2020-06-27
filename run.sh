6f94410447455fd0e0bf6d9a0fb267cfc255a413
hernando.ariza@ormuco.com <hernando.ariza@ormuco.com>
2020-06-27 17:19:12 -0500

    adding run file

---
+++ run.sh
@@ -0,0 +1,14 @@
+
+host_url=$1
+cd /op/blockchain/mypythonblockchain/
+python -m pip install -r requirements.txt
+export FLASK_APP=node_server.py
+python -m flask run --host 0.0.0.0 --port 8000 &
+
+
+export FLASK_APP=run_simulator.py
+export BACKEND_API="http://127.0.0.1:8000"
+python -m flask run --host 0.0.0.0 --port 5000 &
+
+url="http://localhost:5000/run_simulator"            
+curl -X POST $url -d "{'host_url': "$host_url" }" -H "Content-Type: application/json"
